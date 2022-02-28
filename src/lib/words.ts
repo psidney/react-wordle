5440efa2f73ce10be88651841ccaf0c5f2878b20
psidney <patori@gmail.com>
2022-02-28 18:10:08 -0500

    .

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -76,7 +76,7 @@ export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
-  const msInDay = 86400000
+  const msInDay = 360000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
 
